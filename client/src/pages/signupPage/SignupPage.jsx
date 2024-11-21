9882b7b084eaade5cec9ee15745ff15cb5838975
mrmdwatson <mrmdwatson@gmail.com>
2024-11-21 14:07:02 -0500

    remove whitespace

--- client/src/pages/signupPage/SignupPage.jsx
+++ client/src/pages/signupPage/SignupPage.jsx
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
 import "./signupPage.css";
 
 export default function SignupPage() {
-  const dispatch = useDispatch();
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
